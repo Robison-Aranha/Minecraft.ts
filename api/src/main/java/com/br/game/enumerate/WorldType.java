56efda573658439996332f11765064d3c187803a
Rice <62962323+Robison-Aranha@users.noreply.github.com>
2026-03-07 02:14:17 -0300

    Delete api directory

--- api/src/main/java/com/br/game/enumerate/WorldType.java
+++
@@ -1,7 +0,0 @@
-package com.br.game.enumerate;
-
-public enum WorldType {
-    DEFAULT,
-    SUPERFLAT
-
-}
--- api/src/main/java/com/br/game/repository/WorldRepository.java
+++
@@ -1,8 +0,0 @@
-package com.br.game.repository;
-
-import com.br.game.domain.World;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorldRepository extends JpaRepository<World, Long> {
-
-}
